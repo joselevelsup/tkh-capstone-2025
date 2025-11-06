ae1d287bd4c37c2c20c57ddd12262e1613b5c73f
Your Name <you@example.com>
2025-11-05 19:49:21 -0500

    mood tracker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Nav.jsx";
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Journal from "./pages/Journal.jsx";
-import Achievement from "./pages/Achievement.jsx";
-import Shop from "./pages/Shop.jsx";
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from "../Navbar/Nav.jsx"
+import Home from '../pages/Home.jsx';
+import About from '../pages/About.jsx';
+import Journal from '../pages/Journal.jsx';
+import Achievement from '../pages/Achievement.jsx';
+import Shop from '../pages/Shop.jsx';
+import Login from '../pages/Login.jsx';
+import Signup from '../pages/Signup.jsx';
+
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='/shop' element ={<Shop />} />
         <Route path='/login' element ={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/moodtracker' element={<Moodtracker />} />
       </Routes>
     </BrowserRouter>
     </div>
